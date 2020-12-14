13d78b5fb70523bb61c27d272b900589a2d49610
langyongya <470288784@qq.com>
2020-12-14 18:09:25 +0800

    test

--- sc-eureka-service-provider/src/main/java/com/spring/cloud/sceurekaserviceprovider/bean/User.java
+++ sc-eureka-service-provider/src/main/java/com/spring/cloud/sceurekaserviceprovider/bean/User.java
@@ -2,4 +2,11 @@ package com.spring.cloud.sceurekaserviceprovider.bean;
 
 public class User {
 
+    private String name;
+
+    private String address;
+
+    private int age;
+
+    private String sex;
 }
